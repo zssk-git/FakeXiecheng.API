28cdc7e2c57ab4ef4ea99f35afd5523d60527604
shenshaokang zheng <zhengshenshaokang@outlook.com>
2021-04-26 17:14:22 +0800

    用json文件添加表数据

--- FakeXiecheng.API/Database/AppDbContext.cs
+++ FakeXiecheng.API/Database/AppDbContext.cs
@@ -1,8 +1,11 @@
 ﻿using FakeXiecheng.API.Moldes;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace FakeXiecheng.API.Database
@@ -18,15 +21,25 @@ namespace FakeXiecheng.API.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
-            {
-                Id = Guid.NewGuid(),
-                Title = "ceshititle",
-                Description = "shouming",
-                OriginalPrice = 0,
-                CreateTime = DateTime.UtcNow
-            });
+            //modelBuilder.Entity<TouristRoute>().HasData(new TouristRoute()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Title = "ceshititle",
+            //    Description = "shouming",
+            //    OriginalPrice = 0,
+            //    CreateTime = DateTime.UtcNow
+            //});
+
+            var touristRoutesMockData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRoutesMockData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePicturesMockData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePicturesMockData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+
             base.OnModelCreating(modelBuilder);
+
         }
     }
 }
