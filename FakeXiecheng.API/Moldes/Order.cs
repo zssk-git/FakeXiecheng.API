884d0893ad7010112845e015d7dad67bc3e04d45
shenshaokang zheng <zhengshenshaokang@outlook.com>
2021-05-04 17:30:39 +0800

    使用Stateless实现订单状态机

--- FakeXiecheng.API/Moldes/Order.cs
+++ FakeXiecheng.API/Moldes/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Stateless;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,17 +7,35 @@ using System.Threading.Tasks;
 
 namespace FakeXiecheng.API.Moldes
 {
+    /// <summary>
+    /// 订单状态
+    /// </summary>
     public enum OrderStateEnum
     {
-        Peding,//订单已生成
+        Pending,//订单已生成
         Processing,//支付处理中
         Completed,//交易成功
         Declined,//交易失败
         Cancelled,//订单取消
         Refund,//已退款
     }
+    /// <summary>
+    /// 订单状态触发动作
+    /// </summary>
+    public enum OrderStateTriggerEnum
+    {
+        PlaceOrder,//支付
+        Approve,//支付成功
+        Reject,//支付失败
+        Cancel,//取消
+        Return//退货
+    }
     public class Order
     {
+        public Order()
+        {
+            StateMachienInit();
+        }
         [Key]
         public Guid Id { get; set; }
         public string UserId { get; set; }
@@ -26,5 +45,27 @@ namespace FakeXiecheng.API.Moldes
         public OrderStateEnum State { get; set; }
         public DateTime CraeteDateUTC { get; set; }
         public string TransactionMetadata { get; set; }
+
+        StateMachine<OrderStateEnum, OrderStateTriggerEnum> _machine;
+
+        private void StateMachienInit()
+        {
+            _machine = new StateMachine<OrderStateEnum, OrderStateTriggerEnum>(OrderStateEnum.Pending);
+
+            _machine.Configure(OrderStateEnum.Pending)
+                .Permit(OrderStateTriggerEnum.PlaceOrder, OrderStateEnum.Processing)
+                .Permit(OrderStateTriggerEnum.Cancel, OrderStateEnum.Cancelled);
+
+            _machine.Configure(OrderStateEnum.Processing)
+                .Permit(OrderStateTriggerEnum.Approve, OrderStateEnum.Completed)
+                .Permit(OrderStateTriggerEnum.Reject, OrderStateEnum.Declined);
+
+            _machine.Configure(OrderStateEnum.Declined)
+                .Permit(OrderStateTriggerEnum.PlaceOrder, OrderStateEnum.Processing);
+
+            _machine.Configure(OrderStateEnum.Completed)
+                .Permit(OrderStateTriggerEnum.Return, OrderStateEnum.Refund);
+        }
+
     }
 }
